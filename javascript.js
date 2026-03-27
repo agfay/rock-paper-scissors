e8982f27904169bd871fce295133e0ca7cc8d51f
agfay <6284472+agfay@users.noreply.github.com>
2026-03-28 00:01:33 +0800

    Added playRound logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 console.log("Hello World") 
 
+function getHumanRoll () {
+    let humanChoice = prompt("Enter your roll (rock, paper, scissors):");
+    return(humanChoice);
+}
 function getComputerRoll () {
     let computerRoll = Math.trunc(3 * Math.random());
     if (computerRoll == 0) {
@@ -13,10 +17,49 @@ function getComputerRoll () {
     };
 }
 
-function getHumanRoll () {
-    let humanChoice = prompt("Enter your roll (rock, paper, scissors):");
-    return(humanChoice);
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+    console.log("You choose " + humanChoice);
+    console.log("Computer chooses " + computerChoice);
+
+    if(humanChoice == computerChoice){
+        console.log("Tie");
+        return;
+    } else if (humanChoice.charAt(0)+computerChoice.charAt(0) == "rp") {
+        computerScore++;
+        console.log("You lose");
+        return;
+    } else if (humanChoice.charAt(0)+computerChoice.charAt(0) == "rs") {
+        humanScore++;
+        console.log("You win");
+        return;
+    } else if (humanChoice.charAt(0)+computerChoice.charAt(0) == "pr") {
+        humanScore++;
+        console.log("You win");
+        return;
+    } else if (humanChoice.charAt(0)+computerChoice.charAt(0) == "ps") {
+        computerScore++;
+        console.log("You lose");
+        return;
+    } else if (humanChoice.charAt(0)+computerChoice.charAt(0) == "sr") {
+        computerScore++;
+        console.log("You lose");
+        return;
+    } else if (humanChoice.charAt(0)+computerChoice.charAt(0) == "sp") {
+        humanScore++;
+        console.log("You win");
+        return;
+    } else {
+        return("error in playRound")
+    }
 }
 
-console.log(getComputerRoll());
-console.log(getHumanRoll());
+
+const humanSelection = getHumanRoll();
+const computerSelection = getComputerRoll();
+
+playRound(humanSelection, computerSelection);
