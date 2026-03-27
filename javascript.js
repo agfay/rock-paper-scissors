5420ed263235ff6b7a6bcb84c509780f8df650bb
agfay <6284472+agfay@users.noreply.github.com>
2026-03-27 23:31:50 +0800

    Built computer roll

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello World") 
+
+function getComputerRoll () {
+    let computerRoll = Math.trunc(3 * Math.random());
+    if (computerRoll == 0) {
+    return("rock")
+    } else if (computerRoll == 1) {
+    return("paper")
+    } else if (computerRoll == 2) {
+    return("scissors")
+    } else {
+    return("error in computerRoll")
+    }
+}
+
+console.log(getComputerRoll())
